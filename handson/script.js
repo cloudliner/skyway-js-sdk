4d60aafeee6e903167062ca93d78ac76c19b6620
cloudliner <cloudliner.jp@gmail.com>
2018-06-24 14:16:26 +0900

    step-1

--- handson/script.js
+++ handson/script.js
@@ -1,3 +1,19 @@
-$(function(){
-
+$(function() {
+  let localStream = null;
+  navigator.mediaDevices
+    .getUserMedia({
+      audio: true,
+      video: {
+        width: { min: 640, ideal: 1280 },
+        height: { min: 480, ideal: 720 },
+      },
+    })
+    .then(function(stream) {
+      $('#myStream').get(0).srcObject = stream;
+      localStream = stream;
+    })
+    .catch(function(error) {
+      console.error('mediaDevice.getUserMedia() error:', error);
+      return;
+    });
 });
