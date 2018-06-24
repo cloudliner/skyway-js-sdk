031c68a0baa31b3ba0c7e0032621980ba0d3052b
cloudliner <cloudliner.jp@gmail.com>
2018-06-24 16:10:38 +0900

    step-4

--- handson/script.js
+++ handson/script.js
@@ -3,28 +3,57 @@ $(function() {
   let peer = null;
   let exsistingCall = null;
 
-  let constraints = {
-    video: {},
-    audio: true,
-  };
-  constraints.video.width = {
-    min: 320,
-    max: 320,
-  };
-  constraints.video.height = {
-    min: 240,
-    height: 240,
-  };
+  const audioSelect = $('#audioSource');
+  const videoSelect = $('#videoSource');
+
   navigator.mediaDevices
-    .getUserMedia(constraints)
-    .then(function(stream) {
-      $('#myStream').get(0).srcObject = stream;
-      localStream = stream;
+    .enumerateDevices()
+    .then(function(deviceInfos) {
+      for (let i = 0; i !== deviceInfos.length; ++i) {
+        const deviceInfo = deviceInfos[i];
+        const option = $('<option>');
+        option.val(deviceInfo.deviceId);
+        if (deviceInfo.kind === 'audioinput') {
+          option.text(deviceInfo.label);
+          audioSelect.append(option);
+        } else if (deviceInfo.kind === 'videoinput') {
+          option.text(deviceInfo.label);
+          videoSelect.append(option);
+        }
+      }
+      videoSelect.on('change', setupGetUserMedia);
+      audioSelect.on('change', setupGetUserMedia);
+      setupGetUserMedia();
     })
     .catch(function(error) {
-      console.error('mediaDevice.getUserMedia() error:', error);
+      console.error('mediaDevice.enumerateDevices() error:', error);
       return;
     });
+
+  function setupGetUserMedia() {
+    const audioSource = $('#audioSource').val();
+    const videoSource = $('#videoSource').val();
+    const constraints = {
+      audio: { deviceId: { exact: audioSource } },
+      video: { deviceId: { exact: videoSource } },
+    };
+
+    if (localStream) {
+      localStream = null;
+    }
+    navigator.mediaDevices
+      .getUserMedia(constraints)
+      .then(function(stream) {
+        $('#myStream').get(0).srcObject = stream;
+        localStream = stream;
+        if (exsistingCall) {
+          exsistingCall.replaceStream(stream);
+        }
+      }).catch(function(error) {
+        console.error('mediaDevices.getUserMedia() error', error);
+        return;
+      });
+  }
 
   peer = new Peer({
     key: '77c7d4df-e243-42e6-8746-b793d309f3b6',
@@ -86,6 +115,12 @@ $(function() {
     $('#room-id').text(call.name);
     call.on('stream', function(stream) {
       addVideo(call, stream);
+    });
+    call.on('stream', function(stream) {
+      addVideo(stream);
+    });
+    call.on('removeStream', function(stream) {
+      removeVideo(stream.peerId);
     });
     call.on('peerLeave', function(peerId) {
       removeVideo(peerId);
